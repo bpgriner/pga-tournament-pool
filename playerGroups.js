c2bf9e74791ddbb884c28df935a7c747aeec0ace
Ben Griner <ben.griner@sparcedge.com>
2012-04-06 14:33:52 -0400

    player group data for five participants

---
+++ playerGroups.js
@@ -0,0 +1,247 @@
+/**
+ * User: Ben Griner
+ * Start Date: 4/6/12, 2:07 PM
+ */
+
+var playerGroups = [
+    {
+        owner: 'Ben Griner',
+        tieBreakerScore: '-11',
+        groupAplayerOne: {
+            playerId: '',
+            playerName: 'Rory McIlroy'
+        },
+        groupAplayerTwo: {
+            playerId: '',
+            playerName: 'Tiger Woods'
+        },
+        groupAplayerThree: {
+            playerId: '',
+            playerName: 'Phil Mickelson'
+        },
+        groupBplayerOne: {
+            playerId: '',
+            playerName: 'K.J. Choi'
+        },
+        groupBplayerTwo: {
+            playerId: '',
+            playerName: 'Paul Casey'
+        },
+        groupBplayerThree: {
+            playerId: '',
+            playerName: 'Aaron Baddeley'
+        },
+        groupCplayerOne: {
+            playerId: '',
+            playerName: 'Jonathan Byrd'
+        },
+        groupCplayerTwo: {
+            playerId: '',
+            playerName: 'Fred Couples'
+        },
+        groupCplayerThree: {
+            playerId: '',
+            playerName: 'Zach Johnson'
+        },
+        groupCplayerFour: {
+            playerId: '',
+            playerName: 'Sean O\'Hair'
+        },
+        amateurPlayer : {
+            playerId: '',
+            playerName: 'Corbin Mills'
+        }
+    },
+    {
+        owner: 'Evan Smith',
+        tieBreakerScore: '-10',
+        groupAplayerOne: {
+            playerId: '',
+            playerName: 'Tiger Woods'
+        },
+        groupAplayerTwo: {
+            playerId: '',
+            playerName: 'Rory McIlroy'
+        },
+        groupAplayerThree: {
+            playerId: '',
+            playerName: 'Phil Mickelson'
+        },
+        groupBplayerOne: {
+            playerId: '',
+            playerName: 'Brandt Snedeker'
+        },
+        groupBplayerTwo: {
+            playerId: '',
+            playerName: 'Paul Casey'
+        },
+        groupBplayerThree: {
+            playerId: '',
+            playerName: 'Jason Dufner'
+        },
+        groupCplayerOne: {
+            playerId: '',
+            playerName: 'Rickie Fowler'
+        },
+        groupCplayerTwo: {
+            playerId: '',
+            playerName: 'Jim Furyk'
+        },
+        groupCplayerThree: {
+            playerId: '',
+            playerName: 'Zach Johnson'
+        },
+        groupCplayerFour: {
+            playerId: '',
+            playerName: 'Sean O\'Hair'
+        },
+        amateurPlayer : {
+            playerId: '',
+            playerName: 'Patrick Cantlay'
+        }
+    },
+    {
+        owner: 'Trey Adams',
+        tieBreakerScore: '-12',
+        groupAplayerOne: {
+            playerId: '',
+            playerName: 'Rory McIlroy'
+        },
+        groupAplayerTwo: {
+            playerId: '',
+            playerName: 'Tiger Woods'
+        },
+        groupAplayerThree: {
+            playerId: '',
+            playerName: 'Hunter Mahan'
+        },
+        groupBplayerOne: {
+            playerId: '',
+            playerName: 'K.J. Choi'
+        },
+        groupBplayerTwo: {
+            playerId: '',
+            playerName: 'Ian Poulter'
+        },
+        groupBplayerThree: {
+            playerId: '',
+            playerName: 'Louis Oosthuizen'
+        },
+        groupCplayerOne: {
+            playerId: '',
+            playerName: 'Fred Couples'
+        },
+        groupCplayerTwo: {
+            playerId: '',
+            playerName: 'Padraig Harrington'
+        },
+        groupCplayerThree: {
+            playerId: '',
+            playerName: 'Sean O\'Hair'
+        },
+        groupCplayerFour: {
+            playerId: '',
+            playerName: 'Vijay Singh'
+        },
+        amateurPlayer : {
+            playerId: '',
+            playerName: 'Hideki Matsuyama'
+        }
+    },
+    {
+        owner: 'Tripp Brunson',
+        tieBreakerScore: '-13',
+        groupAplayerOne: {
+            playerId: '',
+            playerName: 'Luke Donald'
+        },
+        groupAplayerTwo: {
+            playerId: '',
+            playerName: 'Tiger Woods'
+        },
+        groupAplayerThree: {
+            playerId: '',
+            playerName: 'Phil Mickelson'
+        },
+        groupBplayerOne: {
+            playerId: '',
+            playerName: 'K.J. Choi'
+        },
+        groupBplayerTwo: {
+            playerId: '',
+            playerName: 'David Toms'
+        },
+        groupBplayerThree: {
+            playerId: '',
+            playerName: 'Aaron Baddeley'
+        },
+        groupCplayerOne: {
+            playerId: '',
+            playerName: 'Tim Clark'
+        },
+        groupCplayerTwo: {
+            playerId: '',
+            playerName: 'Ben Crane'
+        },
+        groupCplayerThree: {
+            playerId: '',
+            playerName: 'Padraig Harrington'
+        },
+        groupCplayerFour: {
+            playerId: '',
+            playerName: 'Charles Howell III'
+        },
+        amateurPlayer : {
+            playerId: '',
+            playerName: 'Hideki Matsuyama'
+        }
+    },
+    {
+        owner: 'Roger Winn',
+        tieBreakerScore: '-14',
+        groupAplayerOne: {
+            playerId: '',
+            playerName: 'Luke Donald'
+        },
+        groupAplayerTwo: {
+            playerId: '',
+            playerName: 'Rory McIlroy'
+        },
+        groupAplayerThree: {
+            playerId: '',
+            playerName: 'Tiger Woods'
+        },
+        groupBplayerOne: {
+            playerId: '',
+            playerName: 'Sergio Garcia'
+        },
+        groupBplayerTwo: {
+            playerId: '',
+            playerName: 'K.J. Choi'
+        },
+        groupBplayerThree: {
+            playerId: '',
+            playerName: 'Mark Wilson'
+        },
+        groupCplayerOne: {
+            playerId: '',
+            playerName: 'Rickie Fowler'
+        },
+        groupCplayerTwo: {
+            playerId: '',
+            playerName: 'Jim Furyk'
+        },
+        groupCplayerThree: {
+            playerId: '',
+            playerName: 'Charles Howell III'
+        },
+        groupCplayerFour: {
+            playerId: '',
+            playerName: 'Geoff Ogilvy'
+        },
+        amateurPlayer : {
+            playerId: '',
+            playerName: 'Hideki Matsuyama'
+        }
+    }
+];
